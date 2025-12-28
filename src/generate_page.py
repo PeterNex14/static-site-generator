b43016e237e2c9f49677bf76cbe952516e2020e3
PeterNex14 <peternex42@gmail.com>
2025-12-28 08:57:36 +0800

    deploy to github

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r") as from_file:
@@ -20,6 +20,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_content)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     dest_dir = os.path.dirname(dest_path)
     if dest_dir != "":
@@ -29,12 +31,12 @@ def generate_page(from_path, template_path, dest_path):
         dest_file.write(template)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path) and from_path.endswith(".md"):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         elif os.path.isdir(from_path):
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,28 @@
+import sys
 from generate_page import generate_pages_recursive
 import os
+import sys
 from io_operation import copy_recursive, delete_directory_dst
 
 def main():
+
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
     current_directory = os.path.dirname(__file__)
     project_root = os.path.dirname(current_directory)
 
     static_path = os.path.join(project_root, "static")
-    public_path = os.path.join(project_root, "public")
+    docs_path = os.path.join(project_root, "docs")
+    content_path = os.path.join(project_root, "content")
+    template_path = os.path.join(project_root, "template.html")
 
-    delete_directory_dst(public_path)
-    copy_recursive(static_path, public_path)
-    generate_pages_recursive("content", "template.html", "public")
+
+    delete_directory_dst(docs_path)
+    copy_recursive(static_path, docs_path)
+    generate_pages_recursive(content_path, template_path, docs_path, basepath)
     
 
 
