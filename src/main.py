8023ba8010e2270d1e77c7eb22ad5e2e0e3771c1
PeterNex14 <peternex42@gmail.com>
2025-12-28 08:14:12 +0800

    adding more page to it

--- src/main.py
+++ src/main.py
@@ -12,6 +12,10 @@ def main():
     delete_directory_dst(public_path)
     copy_recursive(static_path, public_path)
     generate_page("content/index.md", "template.html", "public/index.html")
+    generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
+    generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
+    generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
+    generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
     
 
 
